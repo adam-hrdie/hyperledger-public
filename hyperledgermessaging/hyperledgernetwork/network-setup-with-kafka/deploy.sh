dc03192fc582f0341f4bd29689df0af00f45d965
Adam Hardie <adam.hardie@gmex-group.com>
2019-04-29 17:34:21 +0100

    removed sub dir

--- hyperledgermessaging/hyperledgernetwork/network-setup-with-kafka/deploy.sh
+++
@@ -1,7 +0,0 @@
- git archive --remote=ssh://git@forum-subver-01/home/git/forumcrypto.git HEAD |  tar xvf - hyperledgernetwork/network-setup-with-kafka/*
- 
- mv scripts/script.sh .
- 
- chmod 755 script.sh
- 
- ./script.sh
